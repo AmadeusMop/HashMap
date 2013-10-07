c93f1c96451c3fc513d5c53f2de914c54f508164
Cillian Murphy <spacill@murphnduff.org>
2013-10-07 09:40:09 -0700

    Added more GUI stuff

--- HashMap/src/hashMap/Main.java
+++ HashMap/src/hashMap/Main.java
@@ -1,31 +1,45 @@
 package hashMap;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Main {
 	private static final int SIZE = 10;
 	
 	public static void main(String[] args) {
+		Scanner inputFile = new Scanner("inputFile.txt");
 		HashMap2 hashMap = new HashMap2(SIZE);
 		JFrame frame = new JFrame("Hash Map");
-		JPanel grid = new JPanel(new GridLayout(2, 0)), p;
+		JPanel grid = new JPanel(new GridLayout(2, 0));
+		String[] inputLines;
 		for(int i = 0; i < SIZE; i++) {
 			grid.add(new JLabel(Integer.toString(i)));
 		}
 		JPanel[] buckets = addBucketsToPanel(grid);
 		grid.validate();
 		
-		String str1 = "hello";
-		hashMap.add(str1, "goodbye");
-		p = buckets[hashMap.getBucket(str1)];
-		p.add(new JLabel("<html>" + str1 + "<br>" + hashMap.get(str1)));
+		while(inputFile.hasNextLine()) {
+			inputLines = inputFile.nextLine().split(" ");
+			addToBucketsAndHashMap(inputLines[0], inputLines[1], hashMap, buckets);
+		}
+		inputFile.close();
 		
 		frame.setPreferredSize(new Dimension(640, 480));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(grid);
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	public static void addToBucketsAndHashMap(String key, String val, HashMap2 hashMap, JPanel[] buckets) {
+		hashMap.add(key, val);
+		JPanel p = buckets[hashMap.getBucket(key)];
+		p.add(new JLabel("<html>" + key + "<br>" + hashMap.get(key)));
 	}
 	
 	public static JPanel[] addBucketsToPanel(JPanel panel) {
